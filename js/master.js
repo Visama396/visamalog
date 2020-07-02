5dad1f8ef4fa9708dc7b875a8933eac872f9448f
Visama396 <visama396@gmail.com>
2020-07-02 16:31:18 +0200

    First commit

---
+++ js/master.js
@@ -0,0 +1,9 @@
+var options = {
+  strings: ['Hello', 'Hola', 'Hallo', 'こんにちは'],
+  typeSpeed: 70,
+  loop: true,
+  loopCount: Infinity,
+  backSpeed: 60
+};
+
+var typed = new Typed('#typed', options)
---
+++ update.bat
@@ -0,0 +1,2 @@
+scp -r E:\Portfolio\* visamalog@visamalog.com:/home/visamalog/html
+pause
