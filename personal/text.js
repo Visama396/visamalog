c2644be62293c8589fb69f49cb5e730830eee84c
Visama396 <visama396@gmail.com>
2021-03-08 16:45:09 +0100

    Added Skills and About me

--- personal/text.js
+++ personal/text.js
@@ -9,29 +9,34 @@ var typed = new Typed('.typed', options);
 
 document.onreadystatechange = function() {
     if (document.readyState === 'complete') {
-        var pageSections = document.querySelectorAll('body > section');
+
         var navbarLinks = document.querySelectorAll('nav > .menu > a');
+
+        /* Navigator helper */
+        var pageSections = document.querySelectorAll('body > section');
+
+        var sections = {};
+        var i = 0;
+
+        Array.prototype.forEach.call(pageSections, function(e) {
+            sections[e.id] = e.offsetTop;
+        });        
         
-        document.addEventListener("scroll", function() {
-            var scrolled = document.body.scrollTop || document.documentElement.scrollTop;
+        window.onscroll = function() {
+            var scrollPosition = document.body.scrollTop || document.documentElement.scrollTop;
 
-            for (var link of navbarLinks) {
+            /*for (var link of navbarLinks) {
                 link.classList.remove('active');
+            }*/
+
+            for (i in sections) {
+                if (sections[i] <= scrollPosition) {
+                    document.querySelector('a.active').classList.remove('active');
+                    document.querySelector(`a[href*=${i}]`).classList.add('active');
+                }
             }
 
-            if (scrolled > pageSections[4].clientHeight) {
-                navbarLinks[4].classList.add('active');
-            } else if (scrolled > pageSections[3].clientHeight) {
-                navbarLinks[3].classList.add('active');
-            } else if (scrolled > pageSections[2].clientHeight) {
-                navbarLinks[2].classList.add('active');
-            } else if (scrolled > pageSections[1].clientHeight) {
-                navbarLinks[1].classList.add('active');
-            } else {
-                navbarLinks[0].classList.add('active');
-            }
-
-        });
+        };
     }
 }
 
