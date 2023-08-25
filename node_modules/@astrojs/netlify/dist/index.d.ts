e2fb56c8733658740d64968fbbd9817b90a1bab3
Victor <visama396@gmail.com>
2023-08-25 22:46:29 +0200

    Removed Astro

--- node_modules/@astrojs/netlify/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { netlifyEdgeFunctions } from './integration-edge-functions.js';
-export { netlifyFunctions as default, netlifyFunctions } from './integration-functions.js';
-export { netlifyStatic } from './integration-static.js';
